2d84bfd9e7362057918c8219b943004f1738ea04
jx06t <50313tjx06@gmail.com>
2026-09-08 22:57:58 +0800

    Feat: Support dynamic image size in Discriminator and add 128x128 config

--- src/models/discriminator/patch_gan.py
+++ src/models/discriminator/patch_gan.py
@@ -1,27 +1,30 @@
 import torch.nn as nn
+import math
 
 class Discriminator(nn.Module):
     def __init__(self, config):
         super(Discriminator, self).__init__()
         nc = config.nc
         ndf = config.ndf
+        image_size = config.image_size
 
-        self.main = nn.Sequential(
-            # 64 -> 32
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # 32 -> 16
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # 16 -> 8
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # 8 -> 4
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # Output decision: 4 -> 1
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-        )
+        # 計算需要多少次下採樣才能將尺寸降至 4x4
+        # 假設 image_size 是 2 的次方 (如 64, 128, 256)
+        n_downsampling = int(math.log2(image_size)) - 2
+        
+        layers = []
+        in_ch = nc
+        out_ch = ndf
+
+        for i in range(n_downsampling):
+            layers.append(nn.Conv2d(in_ch, out_ch, 4, 2, 1, bias=False))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            in_ch = out_ch
+            out_ch = min(out_ch * 2, 512)  # 最大通道數限制在 512 避免顯存耗盡
+
+        # Output decision: 4 -> 1
+        layers.append(nn.Conv2d(in_ch, 1, 4, 1, 0, bias=False))
+        self.main = nn.Sequential(*layers)
 
     def forward(self, input):
         return self.main(input).view(-1)
